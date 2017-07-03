ed2e5688d4c7b3870ea4ad5a7f01b85f048cfda6
二哥很猛 <664956140@qq.com>
2017-07-03 18:31:12 +0800

    基于ribbon模式调用注册调用方式,基于restTemplate,需要自实现接口

---
+++ spring-cloud-ribbon/src/main/java/com/fanyin/SpringCloudRibbonApplication.java
@@ -0,0 +1,38 @@
+package com.fanyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * EnableDiscoveryClient : 即做服务提供者又做服务消费者
+ * EnableHystrix :添加熔断机制
+ * EnableHystrixDashboard:仪表盘熔断查看 http://ip:port/hystrix
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+public class SpringCloudRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRibbonApplication.class, args);
+	}
+
+	/**
+	 * 该bean作为负载均衡对象
+	 * @return
+	 */
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+
+}
